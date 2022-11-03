5333f2fa52d190e3943fb7ef7dcff5c17b4ea835
ArthurYurkov <77395695+ArthurYurkov@users.noreply.github.com>
2022-11-03 20:56:55 +0200

    Update 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,21 @@
-const startPicker = document.querySelector([data - start]);
-const stopPicker = document.querySelector([data - stop]);
+const startPicker = document.querySelector('[data-start]');
+const stopPicker = document.querySelector('[data-stop]');
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+let intervalId = null;
+
+startPicker.addEventListener('click', event => {
+  intervalId = setInterval(event => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  startPicker.disabled = true;
+});
+
+stopPicker.addEventListener('click', event => {
+  clearInterval(intervalId);
+  startPicker.disabled = false;
+});
