40a630df1702f864a0701eb14358c2fc7b356e82
ArthurYurkov <77395695+ArthurYurkov@users.noreply.github.com>
2022-11-03 18:35:41 +0200

    asd

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,6 @@
+const startPicker = document.querySelector([data - start]);
+const stopPicker = document.querySelector([data - stop]);
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
